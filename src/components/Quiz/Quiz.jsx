03bb366231c40c809b596b5aa9605148f7d57dfc
NikolozMokheve <158738357+NikolozGeladze@users.noreply.github.com>
2025-10-02 18:21:16 +0400

    Finishing My Part

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './Quiz.css';
+import { questions } from '../../data';
 
 export default function Quiz() {
   const [questionsCount, setQuestionsCount] = useState(1);
@@ -12,68 +13,6 @@ export default function Quiz() {
   const answerRefs = useRef([]);
   const errorRef = useRef();
   const btn = useRef()
-  const questions = [
-    {
-      topic: 'HTML',
-      questions: [
-        {
-          question: 'What does HTML stand for?',
-          correctAnswer: 'HyperText Markup Language',
-          answerChoices: [
-            'HyperText Markup Language',
-            'Hyperlinks and Text Markup Language',
-            'Home Tool Markup Language',
-            'Hyper Tool Multi Language'
-          ]
-        },
-        {
-          question: 'Which tag is used to create a link in HTML?',
-          correctAnswer: '<a>',
-          answerChoices: ['<link>', '<a>', '<href>', '<url>']
-        },
-        {
-          question: 'Which tag is used for a paragraph in HTML?',
-          correctAnswer: '<p>',
-          answerChoices: ['<p>', '<paragraph>', '<text>', '<para>']
-        },
-        {
-          question: 'Which attribute is used to specify an image source?',
-          correctAnswer: 'src',
-          answerChoices: ['href', 'link', 'src', 'img']
-        },
-        {
-          question: 'Which HTML tag is used to make a list with bullets?',
-          correctAnswer: '<ul>',
-          answerChoices: ['<ul>', '<ol>', '<li>', '<list>']
-        },
-        {
-          question: 'Which tag is used to make text bold?',
-          correctAnswer: '<b>',
-          answerChoices: ['<bold>', '<b>', '<strong>', '<em>']
-        },
-        {
-          question: 'Which tag is used to create a table in HTML?',
-          correctAnswer: '<table>',
-          answerChoices: ['<table>', '<tab>', '<tr>', '<td>']
-        },
-        {
-          question: 'Which tag is used for a line break?',
-          correctAnswer: '<br>',
-          answerChoices: ['<break>', '<lb>', '<br>', '<hr>']
-        },
-        {
-          question: 'Which tag is used to define a heading?',
-          correctAnswer: '<h1> to <h6>',
-          answerChoices: ['<head>', '<header>', '<h1> to <h6>', '<title>']
-        },
-        {
-          question: 'Which attribute specifies the alternative text for an image?',
-          correctAnswer: 'alt',
-          answerChoices: ['title', 'alt', 'src', 'text']
-        }
-      ]
-    }
-  ];
 
 
   useEffect(() => {
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export   const questions = [
+    {
+      topic: 'HTML',
+      questions: [
+        {
+          question: 'What does HTML stand for?',
+          correctAnswer: 'HyperText Markup Language',
+          answerChoices: [
+            'HyperText Markup Language',
+            'Hyperlinks and Text Markup Language',
+            'Home Tool Markup Language',
+            'Hyper Tool Multi Language'
+          ]
+        },
+        {
+          question: 'Which tag is used to create a link in HTML?',
+          correctAnswer: '<a>',
+          answerChoices: ['<link>', '<a>', '<href>', '<url>']
+        },
+        {
+          question: 'Which tag is used for a paragraph in HTML?',
+          correctAnswer: '<p>',
+          answerChoices: ['<p>', '<paragraph>', '<text>', '<para>']
+        },
+        {
+          question: 'Which attribute is used to specify an image source?',
+          correctAnswer: 'src',
+          answerChoices: ['href', 'link', 'src', 'img']
+        },
+        {
+          question: 'Which HTML tag is used to make a list with bullets?',
+          correctAnswer: '<ul>',
+          answerChoices: ['<ul>', '<ol>', '<li>', '<list>']
+        },
+        {
+          question: 'Which tag is used to make text bold?',
+          correctAnswer: '<b>',
+          answerChoices: ['<bold>', '<b>', '<strong>', '<em>']
+        },
+        {
+          question: 'Which tag is used to create a table in HTML?',
+          correctAnswer: '<table>',
+          answerChoices: ['<table>', '<tab>', '<tr>', '<td>']
+        },
+        {
+          question: 'Which tag is used for a line break?',
+          correctAnswer: '<br>',
+          answerChoices: ['<break>', '<lb>', '<br>', '<hr>']
+        },
+        {
+          question: 'Which tag is used to define a heading?',
+          correctAnswer: '<h1> to <h6>',
+          answerChoices: ['<head>', '<header>', '<h1> to <h6>', '<title>']
+        },
+        {
+          question: 'Which attribute specifies the alternative text for an image?',
+          correctAnswer: 'alt',
+          answerChoices: ['title', 'alt', 'src', 'text']
+        }
+      ]
+    }
+  ];
